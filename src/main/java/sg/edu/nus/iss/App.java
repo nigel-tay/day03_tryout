6dd0161b4c7d7fc24955fbfdcb751696d178dd1f
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-04-25 16:05:18 +0800

    create list and add functionality

---
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -0,0 +1,77 @@
+package sg.edu.nus.iss;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        // Start program with welcome message
+        System.out.println("Welcome to your cart thing 🥵");
+        // Variables
+        List<String> fruitList = new ArrayList<String>();
+        Scanner scan = null;
+        String commandGiven = "";
+        String input = "";
+        List<String> listOfFruits = null;
+
+        // Initialise scanner
+        scan = new Scanner(System.in);
+        try {
+
+            while (!(commandGiven.equals("quit"))) {
+                System.out.print("> ");
+                commandGiven = scan.next();
+                // List
+                if (commandGiven.equals("list")) {
+                    // If list empty print empty message
+                    if (!(fruitList.size() > 0)) {
+                        System.out.println("Your cart is empty 😞");
+                    }
+                    // If not list all items in list
+                    else {
+                        for (int i = 0; i < fruitList.size(); i++) {
+                            System.out.printf("%d. %s\n", i+1, fruitList.get(i));
+                        }
+                    }
+                }
+
+                // Add
+                if (commandGiven.equals("add")) {
+                    // Add everything after first index
+                        input = scan.nextLine();
+                        System.out.println("Input:" + input);
+                        listOfFruits = new ArrayList<String>();
+                        listOfFruits = Arrays.asList(input.replaceAll("\\s+","").split(","));
+                        for (int i = 0; i < listOfFruits.size(); i++) {
+                            // If item already exists, print exist messsage and add the rest of the items
+                            // that are not in the list
+                            if (fruitList.contains(listOfFruits.get(i))) {
+                                System.out.println(listOfFruits.get(i) + " is already in cart");
+                            } else {
+                                fruitList.add(listOfFruits.get(i));
+                                System.out.println(listOfFruits.get(i) + " added!");
+                            }
+                        }
+
+                }
+                // Delete
+                // Delete item at specified index
+                // If index given is greater than list size, print error message
+
+                // Save
+                // if (commandGiven == "save") {
+
+                // }
+            }
+
+        } catch (Exception e) {
+            scan.close();
+            System.out.println(e);
+        }
+        finally {
+            scan.close();
+        }
+    }
+}
